4cab873bb579db610d5dfb22cc73cda784cd2d5c
Aziz Umarov <aziz.umarov.gm@gmail.com>
2023-04-10 07:29:33 +0500

    Add initial Carting Api

---
+++ src/NetMPA.Carting/NetMPA.Carting.Api/Configuration/ConfigureSwaggerOptions.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.ComponentModel;
+using System.Reflection;
+
+public class ConfigureSwaggerOptions
+        : IConfigureNamedOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider provider;
+
+    public ConfigureSwaggerOptions(
+        IApiVersionDescriptionProvider provider)
+    {
+        this.provider = provider;
+    }
+
+    public void Configure(SwaggerGenOptions options)
+    {
+        // add swagger document for every API version discovered
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(
+                description.GroupName,
+                CreateVersionInfo(description));
+        }
+
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    }
+
+    public void Configure(string name, SwaggerGenOptions options)
+    {
+        Configure(options);
+    }
+
+    private OpenApiInfo CreateVersionInfo(
+            ApiVersionDescription description)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = "Carting API",            
+            TermsOfService = new Uri("https://example.com/terms"),
+            Version = description.ApiVersion.ToString(),
+            Contact = new OpenApiContact
+            {
+                Name = "Aziz Umarov",
+                Email = string.Empty,
+                Url = new Uri("https://ru.stackoverflow.com/users/387032/aziz-umarov")
+            },
+            License = new OpenApiLicense
+            {
+                Name = "Use under LICX",
+                Url = new Uri("https://example.com/license")
+            }
+        };
+
+        if (description.IsDeprecated)
+        {
+            info.Description += " This API version has been deprecated.";
+        }
+
+        return info;
+    }
+}
---
+++ src/NetMPA.Carting/NetMPA.Carting.Api/Controllers/CartingController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetMPA.Carting.Bll.Interfaces;
+using NetMPA.Carting.Bll.Models;
+using System.Collections;
+
+namespace NetMPA.Carting.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+
+    [ApiController]
+    public class CartingController : ControllerBase
+    {
+        private readonly ILogger<CartingController> logger;
+        private readonly ICartingService cartingService;
+
+        public CartingController(ICartingService cartingService, ILogger<CartingController> logger)
+        {
+            this.logger = logger;
+            this.cartingService = cartingService;
+        }
+
+        /// <summary>
+        /// Get cart info
+        /// </summary>
+        [HttpGet("{key}")]
+        [MapToApiVersion("1.0")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> GetCartInfo([FromRoute] Guid key)
+        {
+            var result = await this.cartingService.GetCartItems(key);
+
+            if (result == null) return NotFound();
+            return Ok(new { cart_key = key, items = result });
+        }
+
+
+        /// <summary>
+        /// Get cart info
+        /// </summary>
+        [HttpGet("{key}")]
+        [MapToApiVersion("2.0")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<Item>>> GetCartInfoV2([FromRoute] Guid key)
+        {
+            var result = await this.cartingService.GetCartItems(key);
+
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+    }
+}
